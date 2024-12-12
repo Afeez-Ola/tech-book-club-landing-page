975dbd504b8f1e7d5cfcc7b1bedc7139246164d2
Afeez Bolaji <afeezbolajiola@gmail.com>
2024-12-12 06:05:05 +0100

    styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,11 +14,16 @@ module.exports = {
         default: "#E6E1DF",
         light: "#FAF5F3"
       },
+      transparent: 'transparent',
       salmon: {
         dark: "#FEA36F",
         default: "#FFF5EF",
         light: "#FFF5EF"
       },
+      gradient: {
+        dark: "linear-gradient(180deg, #FEA36F 0%, #062630 100%)",
+        light: "linear-gradient(90deg, #FFE2D1 0%, #FFF5EF 100%)",
+      }
     },
     backgroundImage: {
       dark: "linear-gradient(107deg, #FF9A60 -11.37%, #062630 61.84%)",
