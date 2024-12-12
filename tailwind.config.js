7306708d47794d8a69d3216f4a080831c1c2fa05
Afeez Bolaji <afeezbolajiola@gmail.com>
2024-12-12 04:28:48 +0100

    configured the colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,23 @@ module.exports = {
     fontFamily: {
       Inter: ["Inter", "sans-serif"],
       Martian: ["Martian Mono", "monospace"]
+    },
+    colors: {
+      neutral: {
+        verydark: "#062630",
+        dark: "#385159",
+        default: "#E6E1DF",
+        light: "#FAF5F3"
+      }, 
+      salmon: {
+        dark: "#FEA36F",
+        default: "#FFF5EF",
+        light: "#FFF5EF"
+      },
+      Gradient: {
+        dark: "linear-gradient(107deg, #FF9A60 -11.37%, #062630 61.84%)",
+        light: "linear-gradient(90deg, #FFE2D1 0%, #FFF5EF 100%)"
+      }
     }
   },
   plugins: [],
