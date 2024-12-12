ebe8fa92af03fcdaa2ae835d4dd8f4aaee8cf090
Afeez Bolaji <afeezbolajiola@gmail.com>
2024-12-12 05:29:58 +0100

    structuring

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,6 +23,8 @@ module.exports = {
     backgroundImage: {
       dark: "linear-gradient(107deg, #FF9A60 -11.37%, #062630 61.84%)",
       light: "linear-gradient(90deg, #FFE2D1 0%, #FFF5EF 100%)",
+      glow: "url('./assets/images/pattern-glow.svg')",
+      
     }
   },
   plugins: [],
