6186f3289a0f3a63fee42a7edf0d33721116747d
Afeez Bolaji <afeezbolajiola@gmail.com>
2024-12-12 04:32:26 +0100

    done with the configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,18 +13,17 @@ module.exports = {
         dark: "#385159",
         default: "#E6E1DF",
         light: "#FAF5F3"
-      }, 
+      },
       salmon: {
         dark: "#FEA36F",
         default: "#FFF5EF",
         light: "#FFF5EF"
       },
-      Gradient: {
-        dark: "linear-gradient(107deg, #FF9A60 -11.37%, #062630 61.84%)",
-        light: "linear-gradient(90deg, #FFE2D1 0%, #FFF5EF 100%)"
-      }
+    },
+    backgroundImage: {
+      dark: "linear-gradient(107deg, #FF9A60 -11.37%, #062630 61.84%)",
+      light: "linear-gradient(90deg, #FFE2D1 0%, #FFF5EF 100%)",
     }
   },
   plugins: [],
 }
-
