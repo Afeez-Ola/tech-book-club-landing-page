3b788f222c7e97b518c19adb249165a4340a3d96
Afeez Bolaji <afeezbolajiola@gmail.com>
2024-12-13 13:09:24 +0100

    fighting for my life

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,8 +27,7 @@ module.exports = {
       transparent: 'transparent',
       salmon: {
         dark: "#FEA36F",
-        default: "#FFF5EF",
-        light: "#FFF5EF"
+        default: "#FFF5EF"
       },
       gradient: {
         dark: "linear-gradient(180deg, #FEA36F 0%, #062630 100%)",
@@ -38,7 +37,7 @@ module.exports = {
     backgroundImage: {
       dark: "linear-gradient(107deg, #FF9A60 -11.37%, #062630 61.84%)",
       light: "linear-gradient(90deg, #FFE2D1 0%, #FFF5EF 100%)",
-      glow: "url('./assets/images/pattern-glow.svg')",
+      glow: "url('/assets/images/pattern-glow.svg')",
       patternCircle: "url('/assets/images/pattern-circle.png')"
 
     }
